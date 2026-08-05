44e005fd77717dd4f830e325e50586966e789cd0
HuyPham <phamvanhuy1709@gmail.com>
2026-08-05 15:43:31 +0700

    SaiGame C3 Ep23

---
+++ Assets/_Script/Damage/DamageSender.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class DamageSender : SaiMonoBehaviour
+{
+    [SerializeField] protected double damage = 1;
+
+    public virtual void Send(Transform obj)
+    {
+        DamageReceiver damageReceiver = obj.GetComponentInChildren<DamageReceiver>();
+        if (damageReceiver == null) return;
+        this.Send(damageReceiver);
+    }
+
+    public virtual void Send(DamageReceiver damageReceiver)
+    {
+        damageReceiver.Deduct(this.damage);
+        this.DestroyObject();
+    }
+
+    protected virtual void DestroyObject()
+    {
+        Destroy(transform.parent.gameObject);
+    }
+
+
+    public virtual void SetDamage(double damage)
+    {
+        this.damage = damage;
+    }
+}
