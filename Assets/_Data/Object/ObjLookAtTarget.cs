24d2e01c17a2601c0897c45c797e09d43b37069d
HuyPham <phamvanhuy1709@gmail.com>
2026-08-14 10:23:08 +0700

    SaiGame C3 Ep61

--- Assets/_Data/Object/ObjLookAtTarget.cs
+++ Assets/_Data/Object/ObjLookAtTarget.cs
@@ -1,13 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjLookAtTarget : SaiMonoBehaviour
 {
+    [Header("Look At Target")]
     [SerializeField] protected Vector3 targetPosition;
     [SerializeField] protected float rotSpeed = 3f;
 
     protected virtual void FixedUpdate()
     {
-        LookAtTarget();
+        this.LootAtTarget();
     }
 
     public virtual void SetRotSpeed(float speed)
@@ -15,16 +18,16 @@ public class ObjLookAtTarget : SaiMonoBehaviour
         this.rotSpeed = speed;
     }
 
-    protected virtual void LookAtTarget()
+    protected virtual void LootAtTarget()
     {
         Vector3 diff = this.targetPosition - transform.parent.position;
         diff.Normalize();
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
 
         float timeSpeed = this.rotSpeed * Time.fixedDeltaTime;
+        Quaternion targetEuler = Quaternion.Euler(0f, 0f, rot_z);
+        Quaternion currentEuler = Quaternion.Lerp(transform.parent.rotation, targetEuler, timeSpeed);
 
-        Quaternion targetEuler = Quaternion.Euler(0f, 0f, rot_z); 
-        Quaternion currentEuler =  Quaternion. Lerp(transform.parent.rotation, targetEuler, timeSpeed);
         transform.parent.rotation = currentEuler;
     }
 }
